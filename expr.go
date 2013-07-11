4f63b938803543fd5f2e60678d741676348cdb1d
Jack Valmadre <jack.valmadre@gmail.com>
2013-07-11 12:51:32 +1000

    Hid expressions within package

--- expr.go
+++ expr.go
@@ -7,107 +7,107 @@ import "math/rand"
 
 // Addition of two vectors.
 // Lazily evaluated.
-func Plus(x, y Const) PlusExpr { return PlusExpr{x, y} }
+func Plus(x, y Const) Const { return plusExpr{x, y} }
 
-type PlusExpr struct{ X, Y Const }
+type plusExpr struct{ X, Y Const }
 
-func (expr PlusExpr) Size() int        { return expr.X.Size() }
-func (expr PlusExpr) At(i int) float64 { return expr.X.At(i) + expr.Y.At(i) }
+func (expr plusExpr) Size() int        { return expr.X.Size() }
+func (expr plusExpr) At(i int) float64 { return expr.X.At(i) + expr.Y.At(i) }
 
 // Difference between two vectors.
 // Lazily evaluated.
-func Minus(x, y Const) MinusExpr { return MinusExpr{x, y} }
+func Minus(x, y Const) Const { return minusExpr{x, y} }
 
-type MinusExpr struct{ X, Y Const }
+type minusExpr struct{ X, Y Const }
 
-func (expr MinusExpr) Size() int        { return expr.X.Size() }
-func (expr MinusExpr) At(i int) float64 { return expr.X.At(i) - expr.Y.At(i) }
+func (expr minusExpr) Size() int        { return expr.X.Size() }
+func (expr minusExpr) At(i int) float64 { return expr.X.At(i) - expr.Y.At(i) }
 
 // Multiplication by a scalar.
 // Lazily evaluated.
-func Scale(alpha float64, x Const) ScaleExpr { return ScaleExpr{alpha, x} }
+func Scale(alpha float64, x Const) Const { return scaleExpr{alpha, x} }
 
-type ScaleExpr struct {
+type scaleExpr struct {
 	Alpha float64
 	X     Const
 }
 
-func (expr ScaleExpr) Size() int        { return expr.X.Size() }
-func (expr ScaleExpr) At(i int) float64 { return expr.Alpha * expr.X.At(i) }
+func (expr scaleExpr) Size() int        { return expr.X.Size() }
+func (expr scaleExpr) At(i int) float64 { return expr.Alpha * expr.X.At(i) }
 
 // Element-wise multiplication.
 // Lazily evaluated.
-func Multiply(x, y Const) MultiplyExpr { return MultiplyExpr{x, y} }
+func Multiply(x, y Const) Const { return multiplyExpr{x, y} }
 
-type MultiplyExpr struct{ X, Y Const }
+type multiplyExpr struct{ X, Y Const }
 
-func (expr MultiplyExpr) Size() int        { return expr.X.Size() }
-func (expr MultiplyExpr) At(i int) float64 { return expr.X.At(i) * expr.Y.At(i) }
+func (expr multiplyExpr) Size() int        { return expr.X.Size() }
+func (expr multiplyExpr) At(i int) float64 { return expr.X.At(i) * expr.Y.At(i) }
 
 // Constant vector of ones.
-func One(n int) OneExpr { return OneExpr{n} }
+func One(n int) Const { return oneExpr(n) }
 
-type OneExpr struct{ N int }
+type oneExpr int
 
-func (expr OneExpr) Size() int      { return expr.N }
-func (expr OneExpr) At(int) float64 { return 1 }
+func (expr oneExpr) Size() int      { return int(expr) }
+func (expr oneExpr) At(int) float64 { return 1 }
 
 // Constant vector of zeros.
-func Zero(n int) ZeroExpr { return ZeroExpr{n} }
+func Zero(n int) Const { return zeroExpr(n) }
 
-type ZeroExpr struct{ N int }
+type zeroExpr int
 
-func (expr ZeroExpr) Size() int      { return expr.N }
-func (expr ZeroExpr) At(int) float64 { return 0 }
+func (expr zeroExpr) Size() int      { return int(expr) }
+func (expr zeroExpr) At(int) float64 { return 0 }
 
 // Constant vector.
-func Constant(n int, x float64) ConstantExpr { return ConstantExpr{n, x} }
+func Constant(n int, x float64) Const { return constantExpr{n, x} }
 
-type ConstantExpr struct {
+type constantExpr struct {
 	N int
 	X float64
 }
 
-func (expr ConstantExpr) Size() int      { return expr.N }
-func (expr ConstantExpr) At(int) float64 { return expr.X }
+func (expr constantExpr) Size() int      { return expr.N }
+func (expr constantExpr) At(int) float64 { return expr.X }
 
 // Applies the same unary function to every element.
 // Lazily evaluated.
-func Map(x Const, f func(int, float64) float64) MapExpr { return MapExpr{x, f} }
+func Map(x Const, f func(int, float64) float64) Const { return mapExpr{x, f} }
 
-type MapExpr struct {
+type mapExpr struct {
 	X Const
 	F func(int, float64) float64
 }
 
-func (expr MapExpr) Size() int        { return expr.X.Size() }
-func (expr MapExpr) At(i int) float64 { return expr.F(i, expr.X.At(i)) }
+func (expr mapExpr) Size() int        { return expr.X.Size() }
+func (expr mapExpr) At(i int) float64 { return expr.F(i, expr.X.At(i)) }
 
 // Applies the same binary function to every element.
 // Lazily evaluated.
-func BinaryMap(x, y Const, f func(int, float64, float64) float64) BinaryMapExpr {
-	return BinaryMapExpr{x, y, f}
+func BinaryMap(x, y Const, f func(int, float64, float64) float64) Const {
+	return binaryMapExpr{x, y, f}
 }
 
-type BinaryMapExpr struct {
+type binaryMapExpr struct {
 	X Const
 	Y Const
 	F func(int, float64, float64) float64
 }
 
-func (expr BinaryMapExpr) Size() int        { return expr.X.Size() }
-func (expr BinaryMapExpr) At(i int) float64 { return expr.F(i, expr.X.At(i), expr.Y.At(i)) }
+func (expr binaryMapExpr) Size() int        { return expr.X.Size() }
+func (expr binaryMapExpr) At(i int) float64 { return expr.F(i, expr.X.At(i), expr.Y.At(i)) }
 
 // Lazily concatenates two vectors.
-func Cat(x, y Const) CatExpr {
-	return CatExpr{x, y}
+func Cat(x, y Const) Const {
+	return catExpr{x, y}
 }
 
-type CatExpr struct{ X, Y Const }
+type catExpr struct{ X, Y Const }
 
-func (expr CatExpr) Size() int { return expr.X.Size() + expr.Y.Size() }
+func (expr catExpr) Size() int { return expr.X.Size() + expr.Y.Size() }
 
-func (expr CatExpr) At(i int) float64 {
+func (expr catExpr) At(i int) float64 {
 	n := expr.X.Size()
 	if i < n {
 		return expr.X.At(i)
@@ -116,15 +116,15 @@ func (expr CatExpr) At(i int) float64 {
 }
 
 // Lazily concatenates two mutable vectors.
-func MutableCat(x, y Mutable) MutableCatExpr {
-	return MutableCatExpr{x, y}
+func MutableCat(x, y Mutable) Mutable {
+	return mutableCatExpr{x, y}
 }
 
-type MutableCatExpr struct{ X, Y Mutable }
+type mutableCatExpr struct{ X, Y Mutable }
 
-func (expr MutableCatExpr) Size() int { return expr.X.Size() + expr.Y.Size() }
+func (expr mutableCatExpr) Size() int { return expr.X.Size() + expr.Y.Size() }
 
-func (expr MutableCatExpr) At(i int) float64 {
+func (expr mutableCatExpr) At(i int) float64 {
 	n := expr.X.Size()
 	if i < n {
 		return expr.X.At(i)
@@ -132,7 +132,7 @@ func (expr MutableCatExpr) At(i int) float64 {
 	return expr.Y.At(i - n)
 }
 
-func (expr MutableCatExpr) Set(i int, x float64) {
+func (expr mutableCatExpr) Set(i int, x float64) {
 	n := expr.X.Size()
 	if i < n {
 		expr.X.Set(i, x)
@@ -142,12 +142,9 @@ func (expr MutableCatExpr) Set(i int, x float64) {
 }
 
 // Vector whose entries are random and normally distributed.
-func Randn(n int) RandnExpr { return RandnExpr(n) }
+func Randn(n int) Const { return randnExpr(n) }
 
-type RandnExpr int
+type randnExpr int
 
-func (expr RandnExpr) Size() int { return int(expr) }
-
-func (expr RandnExpr) At(int) float64 {
-	return rand.NormFloat64()
-}
+func (expr randnExpr) Size() int      { return int(expr) }
+func (expr randnExpr) At(int) float64 { return rand.NormFloat64() }
