0a30bef91f7a62cae172a985997320c0f84873c4
Jack Valmadre <jack.valmadre@gmail.com>
2013-07-11 11:05:28 +1000

    Expanded on documentation

--- doc.go
+++ doc.go
@@ -1,28 +1,41 @@
 /*
 This package provides common functionality for working with real, dense matrices, including BLAS and LAPACK bindings.
 
-A number of operations which could be performed using the vector library are provided, with an attached size to allow
+The core of the package is in the Const and Mutable interfaces. One of the key methods is Copy(Mutable, Const).
+
+Then there are a number of concrete implementations (Contiguous, SubContiguous, NonContiguous) of Mutable, corresponding to different modes of storage.
+Different modes enable different manipulations.
+
+There are a number of thin wrappers for doing simple operations.
+For example, converting between a matrix and a vector using Vec(), Mat(), Reshape(), simple arithmetic operations such as Plus() and Minus(), and matrix shape operations such as Submatrix(), Column().
+These wrappers all return Const vectors or matrices, and are designed for idiomatic use with Copy(), like in the vector library. For example,
+	mat.Copy(C, mat.Plus(A, B))
+
+	// In-place version:
+	mat.Copy(A, mat.Plus(A, B))
+Although it's important to be aware of when an operation actually can be performed in-place! This is more common than in the vector library.
+	// World's ugliest way to ensure A is symmetric?
+	mat.Copy(A, A.T())
+The simple arithmetic operations are mostly thin wrappers around the vector arithmetic operations, allowing
 	C := mat.MakeCopy(mat.Plus(A, B))
 
 	// Instead of:
-	C := mat.Make(Rows(A), Cols(A))
+	C := mat.Make(mat.Rows(A), mat.Cols(A))
 	vec.Copy(C.Vec(), vec.Plus(A.Vec(), B.Vec()))
 
 	// Or:
-	C := mat.MakeCopy(mat.Reshape(vec.Plus(A.Vec(), B.Vec())), Rows(A), Cols(A))
-As in the vector library, these operations lead to a neat pattern for doing in-place calculations using existing memory:
-	mat.Copy(A, mat.Plus(A, B))
+	C := mat.MakeCopy(mat.Reshape(vec.Plus(A.Vec(), B.Vec())), mat.Rows(A), mat.Cols(A))
+Note that the concrete type idiomatically provide T() and Vec() methods which return mutable wrappers for succinctness.
+	mat.Copy(A.Vec(), B.Vec())
 
-	// Or:
-	vec.Copy(A.Vec(), vec.Plus(A.Vec(), B.Vec()))
-
-Mutable versions of these operations allow interesting syntax such as
+	// As opposed to:
+	mat.Copy(mat.MutableVec(A), mat.Vec(B))
+Mutable versions of these operations allow interesting syntax, such as
 	mat.Copy(C.T(), mat.Times(A, B))
 
 	// Equivalent to:
 	mat.Copy(C, mat.Times(B.T(), A.T()))
-But, beware, it is easy to make a mistake.
-	// World's ugliest way to ensure A is symmetric?
-	mat.Copy(A, A.T())
+
+Note that Mat(x) converts a length-n vector to an nx1 matrix and Reshape(x, m, n) converts an (mn)-vector to an mxn matrix.
 */
 package mat
