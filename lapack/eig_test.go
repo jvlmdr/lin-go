a830d6bef7ce42071f0fd549ef61fb90b1c3b185
Jack Valmadre <jack.valmadre@gmail.com>
2013-11-12 17:09:33 +1000

    Add eig test

--- lapack/eig_test.go
+++ lapack/eig_test.go
@@ -2,9 +2,12 @@ package lapack
 
 import (
 	"github.com/jackvalmadre/lin-go/mat"
+	"github.com/jackvalmadre/lin-go/vec"
 
 	"fmt"
+	"math"
 	"sort"
+	"testing"
 )
 
 // eig([7, -2, 0; -2, 6, -2; 0, -2, 5])
@@ -30,4 +33,41 @@ func ExampleEigSymm() {
 	fmt.Printf("(%.6g, %.6g, %.6g)\n", v[0], v[1], v[2])
 	// Output:
 	// (3, 6, 9)
+}
+
+func TestEigSymm_SumVsTrace(t *testing.T) {
+	const n = 100
+	A := mat.MakeStrideCopy(mat.Randn(n, n))
+
+	DA := mat.MakeStride(n, n)
+	d := vec.MakeSliceCopy(vec.Randn(n))
+	for i := 0; i < n; i++ {
+		d.Set(i, d.At(i)*float64(n-i)/float64(n))
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			DA.Set(i, j, d.At(i)*A.At(i, j))
+		}
+	}
+	A = mat.MakeStrideCopy(mat.Times(A.T(), DA))
+
+	eigs, err := EigSymm(A)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var tr float64
+	for i := 0; i < n; i++ {
+		tr += A.At(i, i)
+	}
+
+	var sum float64
+	for _, eig := range eigs {
+		sum += eig
+	}
+
+	const eps = 0
+	if math.Abs(sum-tr) > eps {
+		t.Errorf("trace %g, sum eigs %g", tr, sum)
+	}
 }
